65c7ca6be27ab72ffdfed97965caf05a827306c4
soraya <soraya@DESKTOP-53P974O>
2025-02-07 13:37:00 +0100

    monstruos

---
+++ src/juegocompiladoroscuro/Monstruos.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegocompiladoroscuro;
+
+/**
+ *
+ * @author soraya
+ */
+public class Monstruos {
+
+    //atributos
+    protected int vida;
+    protected int fuerza;
+    protected int defensa;
+    protected int habilidad;
+    protected int velocidad;
+    protected int nivel;
+
+    //constructor
+    public Monstruos(int vida, int fuerza, int defensa, int habilidad, int velocidad, int nivel) {
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+        this.habilidad = habilidad;
+        this.velocidad = velocidad;
+        this.vida = vida;
+    }
+
+    //metodos principales
+    public void atacar() {
+
+    }
+
+    public void recibirDaño() {
+
+    }
+}
