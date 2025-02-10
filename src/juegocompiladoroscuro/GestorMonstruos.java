dc4ee5d0c0f4c0a82b694070e336445eb8ed9229
sorayateran <lapaa10z1010@gmail.com>
2025-02-10 08:22:41 +0100

    Update GestorMonstruos.java

--- src/juegocompiladoroscuro/GestorMonstruos.java
+++ src/juegocompiladoroscuro/GestorMonstruos.java
@@ -8,12 +8,32 @@ package juegocompiladoroscuro;
  *
  * @author soraya
  */
-public class GestorMonstruos {
-    
-    
+public class GestorMonstruos extends Monstruos {
+
+    protected Monstruos[] monstruos;
+    protected int contadorMonstruos;
+
+    //constructor
+    public GestorMonstruos(int vida, int fuerza, int defensa, int habilidad, int velocidad, int nivel) {
+        super(vida, fuerza, defensa, habilidad, velocidad, nivel);
+        monstruos = new Monstruos[10]; //la capacidad del array es de 10
+        this.contadorMonstruos = 0; //de momento no hay monstruos
+    }
 
     //metodos principales
     public void generarMonstruos() {
+        //crear monstruos
+        Monstruos Tiefling = new Monstruos(10, 20, 5, 7, 8, 4);
+        Monstruos Orco = new Monstruos(10, 20, 5, 7, 8, 4);
+        Monstruos OsoLechuza = new Monstruos(10, 20, 5, 7, 8, 4);
+        Monstruos Liche = new Monstruos(10, 20, 5, 7, 8, 4);
+        Monstruos Kobold = new Monstruos(10, 20, 5, 7, 8, 4);
+        //almacenar monstruos en el array
+        monstruos[contadorMonstruos++] = Tiefling;
+        monstruos[contadorMonstruos++] = Orco;
+        monstruos[contadorMonstruos++] = OsoLechuza;
+        monstruos[contadorMonstruos++] = Liche;
+        monstruos[contadorMonstruos++] = Kobold;
 
     }
 
