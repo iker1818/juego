098339b897bb0b38769eb14f17e3c4a9c7b34e1a
iker1818 <ikerocamundo@gmail.com>
2025-02-11 12:03:37 +0100

    Update GestorMonstruos.java

--- src/juegocompiladoroscuro/GestorMonstruos.java
+++ src/juegocompiladoroscuro/GestorMonstruos.java
@@ -11,32 +11,47 @@ package juegocompiladoroscuro;
 public class GestorMonstruos implements interfazGestorMonstruo {
 
     protected Monstruos[] monstruos;
-    protected int aumentoNivel = 0;
+    protected static int contador = 0;
 
     //constructor
     public GestorMonstruos() {
-        monstruos = new Monstruos[5]; //la capacidad del array es de 10
+        monstruos = new Monstruos[10]; //la capacidad del array es de 10
     }
 
     //metodos principales
     public void generarMonstruo(int nivel) {
-        nivel = 1;
+       
         //crear monstruos
         Monstruos monstruo = new Monstruos(nivel * 10, nivel, nivel, nivel, nivel, nivel);
-        monstruos[aumentoNivel] = monstruo;
-        aumentoNivel++;
+        monstruos[contador] = monstruo;
+        contador++;
         System.out.println("Monstruo Nivel; " + nivel);
 
     }
 
-    @Override
     public void eliminarMonstruo(Monstruos monstruo) {
         if (monstruo.vida <= 0) {
             System.out.println("Monstruo eliminado");
-            monstruos[aumentoNivel] = monstruo;
-            aumentoNivel--;
+            monstruos[contador] = monstruo;
+            contador--;
 
         }
     }
 
+    public Monstruos[] getMonstruos() {
+        return monstruos;
+    }
+
+    public void setMonstruos(Monstruos[] monstruos) {
+        this.monstruos = monstruos;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+    
 }
