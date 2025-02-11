5631fb67d4e072df88132c162295748d33f47a87
sorayateran <lapaa10z1010@gmail.com>
2025-02-11 12:15:57 +0100

    Update Monstruos.java

--- src/juegocompiladoroscuro/Monstruos.java
+++ src/juegocompiladoroscuro/Monstruos.java
@@ -27,60 +27,12 @@ public class Monstruos {
         this.vida = vida;
     }
 
-    public int getVida() {
-        return vida;
-    }
-
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
-    public int getFuerza() {
-        return fuerza;
-    }
-
-    public void setFuerza(int fuerza) {
-        this.fuerza = fuerza;
-    }
-
-    public int getDefensa() {
-        return defensa;
-    }
-
-    public void setDefensa(int defensa) {
-        this.defensa = defensa;
-    }
-
-    public int getHabilidad() {
-        return habilidad;
-    }
-
-    public void setHabilidad(int habilidad) {
-        this.habilidad = habilidad;
-    }
-
-    public int getVelocidad() {
-        return velocidad;
-    }
-
-    public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
-    }
-
-    public int getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
     //metodos principales
-    public void atacar(Monstruos valientes) {
+    public void atacar(Monstruos valiente) {
         //hace daño dependiendo del nivel y fuerza del mosntruo
         int danio = (this.fuerza * this.nivel);
         if (danio > 0) { //si el daño es mas que 0 hace daño
-            valientes.recibirDaño(danio);
+            valiente.recibirDaño(danio);
             System.out.println("El monstruo ha hecho: " + danio + "daño");
         } else {
             System.out.println("0 daño"); //si no decimos que ha pegado 0 de daño
