b60ff59895e2a8bd8ef96155cefdc403b4b5e698
iker1818 <ikerocamundo@gmail.com>
2025-02-11 10:13:20 +0100

    Update CompiladorOscuro.java

--- src/juegocompiladoroscuro/CompiladorOscuro.java
+++ src/juegocompiladoroscuro/CompiladorOscuro.java
@@ -8,6 +8,27 @@ package juegocompiladoroscuro;
  *
  * @author soraya
  */
-public class CompiladorOscuro {
-    
+public class CompiladorOscuro implements interfazCompiladorOscuro{
+    protected int vida;
+    protected int Fuerza;
+    protected int Defensa;
+    protected int Habilidad;
+    protected int Velocidad;
+
+    public JuegoCompiladorOscuro() {
+        this.vida = 150;
+        this.Fuerza = 0;
+        this.Defensa = 0;
+        this.Habilidad = 0;
+        this.Velocidad = 0;
+    }
+
+    public void aumentarEstadisticas() {
+        int cantidad = GestorMonstruos.getContador();
+        int aumento = cantidad * 3;
+        this.Fuerza = aumento;
+        this.Defensa = aumento;
+        this.Habilidad = aumento;
+        this.Velocidad = aumento;
+    }
 }
