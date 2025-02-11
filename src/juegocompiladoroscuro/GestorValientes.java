1bc780b37342a918e69ba8665685f86198a08f35
iker1818 <ikerocamundo@gmail.com>
2025-02-11 12:04:24 +0100

    Update GestorValientes.java

--- src/juegocompiladoroscuro/GestorValientes.java
+++ src/juegocompiladoroscuro/GestorValientes.java
@@ -10,33 +10,33 @@ package juegocompiladoroscuro;
  */
 public class GestorValientes implements interfazGestorValientes{
 
-    protected Valiente[] valientes;
+    protected Valientes[] valientes;
     protected int contador = 0;
 
     //Constructor
     public GestorValientes() {
-        valientes = new Valiente[5];
+        valientes = new Valientes[5];
     }
     
     //Crea un valiente de cada tipo, los atributos iniciales suman un total de 40 puntos
     @Override
     public void crearValientesIniciales(){
-        Valiente guerrero = new Juego(60, 15, 12, 5, 8, arma, escudo, 1);
+        Valientes guerrero = new Valientes(60, 15, 12, 5, 8, null, null, 1);
         valientes[contador] = guerrero;
         System.out.println("---------Guerrero--------\nNivel: " + valientes[contador].nivel + "\nVida: " + valientes[contador].vida + "\nFuerza: " + valientes[contador].fuerza + "\nDefensa: " + valientes[contador].defensa + "\nHabilidad: " + valientes[contador].habilidad + "\nVelocidad: " + valientes[contador].velocidad + "\nObjetos: ");
         contador++;
         
-        Valiente paladin = new Juego(60, 12, 15, 5, 8, arma, escudo, 1);
+        Valientes paladin = new Valientes(60, 12, 15, 5, 8, null, null, 1);
         valientes[contador] = paladin;
         System.out.println("---------Paladin--------\nNivel: " + valientes[contador].nivel + "\nVida: " + valientes[contador].vida + "\nFuerza: " + valientes[contador].fuerza + "\nDefensa: " + valientes[contador].defensa + "\nHabilidad: " + valientes[contador].habilidad + "\nVelocidad: " + valientes[contador].velocidad + "\nObjetos: ");
         contador++;
         
-        Valiente mago = new Juego(40, 9, 5, 15, 11, arma, escudo, 1);
+        Valientes mago = new Valientes(40, 9, 5, 15, 11, null, null, 1);
         valientes[contador] = mago;
         System.out.println("---------Mago--------\nNivel: " + valientes[contador].nivel + "\nVida: " + valientes[contador].vida + "\nFuerza: " + valientes[contador].fuerza + "\nDefensa: " + valientes[contador].defensa + "\nHabilidad: " + valientes[contador].habilidad + "\nVelocidad: " + valientes[contador].velocidad + "\nObjetos: ");
         contador++;
         
-        Valiente picaro = new Juego(40, 6, 7, 12, 15, arma, escudo, 1);
+        Valientes picaro = new Valientes(40, 6, 7, 12, 15, null, null, 1);
         valientes[contador] = picaro;
         System.out.println("---------Picaro--------\nNivel: " + valientes[contador].nivel + "\nVida: " + valientes[contador].vida + "\nFuerza: " + valientes[contador].fuerza + "\nDefensa: " + valientes[contador].defensa + "\nHabilidad: " + valientes[contador].habilidad + "\nVelocidad: " + valientes[contador].velocidad + "\nObjetos: ");
         contador++;
