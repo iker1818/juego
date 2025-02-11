b69ff8f33e270f99e7906de51753618af1f4d99d
Adriana-002 <agomezs20@educantabria.es>
2025-02-11 04:47:35 +0100

    Update Valientes.java

--- src/juegocompiladoroscuro/Valientes.java
+++ src/juegocompiladoroscuro/Valientes.java
@@ -8,6 +8,71 @@ package juegocompiladoroscuro;
  *
  * @author soraya
  */
-public class Valientes {
+public class Valientes implements interfazPersonaje, interfazValiente{
     
+    //Atributos
+    protected int vida;
+    protected int fuerza;
+    protected int defensa;
+    protected int habilidad;
+    protected int velocidad;
+    protected Objeto arma;
+    protected Objeto escudo;
+    protected int nivel;
+    
+    //Constructor
+    public Juego(int vida, int fuerza, int defensa, int habilidad, int velocidad, Objeto arma, Objeto escudo, int nivel) {
+        this.vida = vida;
+        this.fuerza = fuerza;
+        this.defensa = defensa;
+        this.habilidad = habilidad;
+        this.velocidad = velocidad;
+        this.arma = arma;
+        this.escudo = escudo;
+        this.nivel = nivel;
+    }
+    
+    //Métodos
+    //Calcula y aplica el daño al enemigo
+    public void atacar(Monstruo enemigo) {      
+        int danio = (this.fuerza * this.nivel);
+        if (danio > 0) {
+            Monstruo.recibirDaño(danio);
+            System.out.println("El monstruo ha hecho: " + danio + " daño");
+        } else {
+            System.out.println("0 daño");
+        }
+    }
+    
+    //Restar vida al valiende al recibir daño
+    public void recibirDaño(int cantidad){
+        this.vida -= cantidad;
+        if (this.vida < 0) 
+            this.vida = 0; //En caso de vida negtiva se establece a 0       
+        System.out.println("El valiente tiene: " + this.vida + " de vida.");
+    }
+    
+    //Ejecuta un ataque especial según la clase del valiente
+    @Override
+    public void usarHhabilidadEspecial(Monstruo enemigo){
+        int danio = (this.fuerza * this.nivel);
+        if (danio > 0) {
+            Monstruo.recibirDaño(danio);
+            System.out.println("El monstruo ha hecho: " + danio + " daño");
+        } else {
+            System.out.println("0 daño");
+        }
+    }
+
+    //Mejora atributos del valiente tras ganar experiencia
+    @Override
+    public void subirNivel(){
+        nivel++;
+        //Sube los puntos necesarios de cada atributo (en caso de límite se establece al máximo)
+        vida = (vida + 10 > 100) ? 100 : vida + 10; 
+        fuerza = (fuerza >= 20) ? 20 : ++fuerza;
+        defensa = (defensa >= 20) ? 20 : ++defensa;
+        habilidad = (habilidad >= 20) ? 20 : ++habilidad;
+        velocidad = (velocidad >= 20) ? 20 : ++velocidad;
+    }
 }
