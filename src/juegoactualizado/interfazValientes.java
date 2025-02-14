671f45312f1605dbf84adab0e4912aeb57b87fd5
Adriana-002 <agomezs20@educantabria.es>
2025-02-14 13:18:39 +0100

    Update interfazValientes.java

--- src/juegoactualizado/interfazValientes.java
+++ src/juegoactualizado/interfazValientes.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package juegoactualizado;
+package juegocompiladoroscuro;
 
 /**
  *
  * @author soraya
  */
 public interface interfazValientes {
-    
+
+    public void habilidadEspecial(Monstruo monstruo);
+
+    public void subirNivel();
+
 }
