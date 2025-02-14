faf7d5c8c9010bab8183241c63e9131b6860e342
iker1818 <ikerocamundo@gmail.com>
2025-02-14 13:02:29 +0100

    Update Escudo.java

--- src/juegoactualizado/Escudo.java
+++ src/juegoactualizado/Escudo.java
@@ -8,6 +8,17 @@ package juegoactualizado;
  *
  * @author soraya
  */
-public class Escudo {
+public class Escudo extends Objeto{
+
+    private int aumentoDefensa;
     
+    public Escudo(String nombre, int aumentoDefensa) {
+        super(nombre);
+        this.aumentoDefensa = aumentoDefensa;
+    }
+
+    public void equiparObjeto(Valientes valiente) {
+        valiente.defensa(aumentoDefensa);
+        System.out.println(valiente.getNombre() + " ha equipado " + nombre + " y ha aumentado su defensa en " + aumentoDefensa);
+    }
 }
