b12d9527fe0d5d58d9fe68bacc95b51841469530
sorayateran <lapaa10z1010@gmail.com>
2025-02-14 13:02:26 +0100

    Update interfazCombate.java

--- src/juegoactualizado/interfazCombate.java
+++ src/juegoactualizado/interfazCombate.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package juegoactualizado;
+package juegocompiladoroscuro;
 
 /**
  *
  * @author soraya
  */
 public interface interfazCombate {
-    
+ 
+ public void  iniciarCombate(Valientes valiente ,Monstruos monstruo);
+ public void turno(Personaje atacante, Personaje defensor);
+ public void terminarCombate(Valientes valiente ,Monstruos monstruo);
+
 }
