c99cf98961c0e031afafc20fdd8c351118b547a2
Adriana-002 <agomezs20@educantabria.es>
2025-02-14 13:33:34 +0100

    Update interfazJuego.java

--- src/juegoactualizado/interfazJuego.java
+++ src/juegoactualizado/interfazJuego.java
@@ -9,5 +9,11 @@ package juegoactualizado;
  * @author soraya
  */
 public interface interfazJuego {
-    
+ 
+ public void iniciarJuego();
+ public void creacionOEleccionValiente();
+ public void mostrarMenuPrincipal();
+ public void explorarMapa(int x, int y);
+ public void mostrarEstadoJuego();
+
 }
