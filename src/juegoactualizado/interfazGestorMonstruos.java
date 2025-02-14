73e05f76d3cf44d746cde300a59ec1d8063471e5
sorayateran <lapaa10z1010@gmail.com>
2025-02-14 13:02:53 +0100

    Update interfazGestorMonstruos.java

--- src/juegoactualizado/interfazGestorMonstruos.java
+++ src/juegoactualizado/interfazGestorMonstruos.java
@@ -2,12 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package juegoactualizado;
+package juegocompiladoroscuro;
 
 /**
  *
  * @author soraya
  */
-public interface interfazGestorMonstruos {
-    
+
+ public interface interfazGestorMonstruo {
+
+ public void generarMonstruo(int nivel);
+ public void eliminarMonstruo(Monstruos monstruo);
+
 }
+ 
